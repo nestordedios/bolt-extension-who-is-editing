95c2c9a38cab244cd0aa52c0ac0b03b5871f1a7f
nestordedios <nestordedios@outlook.es>
2018-03-09 17:06:53 +0100

    Make AJAX calls when data is defined

--- web/who-is-editing.js
+++ web/who-is-editing.js
@@ -1,31 +1,35 @@
 $(document).ready(function(){
 
-    window.setInterval(function(){
-        updateWidget();
-    }, 3000);
+    if(typeof whoiseditingdata != 'undefined') {
+        window.setInterval(function(){
+            updateWidget();
+        }, 3000);
 
-    $( window ).on('beforeunload', function() {
-        updateWidget('close');
-    });
+        $( window ).on('beforeunload', function() {
+            updateWidget('close');
+        });
 
-    $( window ).load(function() {
-        updateWidget('editcontent');
-    });
+        $( window ).load(function() {
+            updateWidget('editcontent');
+        });
+    }
 
     function updateWidget(action = 'editcontent'){
 
         var url = $('.actions-container').attr('data-actions-url');
 
+        var requestData = {
+            // whoiseditingdata array is populated in actions_widget.twig
+            recordID: whoiseditingdata["recordID"],
+            contenttype: whoiseditingdata["contenttype"],
+            userID: whoiseditingdata["userID"],
+            action: action,
+        }
+
         var request = $.ajax({
             url: url,
             method: "GET",
-            data: {
-                // whoiseditingdata array is populated in actions_widget.twig
-                recordID: whoiseditingdata["recordID"],
-                contenttype: whoiseditingdata["contenttype"],
-                userID: whoiseditingdata["userID"],
-                action: action,
-            },
+            data: requestData,
             dataType: "html"
         });
 
@@ -38,7 +42,6 @@ $(document).ready(function(){
         });
 
         request.fail(function( jqXHR, textStatus, errorThrown ) {
-            alert("The request has failed!");
         });
     }
 
